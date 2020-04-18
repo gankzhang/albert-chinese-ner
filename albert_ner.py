72eb09ee8b4d4b8304f94a8278ca382f651e0821
gankzhang <peining8@163.com>
2020-04-18 12:42:08 -0400

    log_step

--- albert_ner.py
+++ albert_ner.py
@@ -682,6 +682,7 @@ def main(_):
       master=FLAGS.master,
       model_dir=FLAGS.output_dir,
       save_checkpoints_steps=FLAGS.save_checkpoints_steps,
+      log_step_count_steps=FLAGS.iterations_per_hook_output,
       tpu_config=tf.contrib.tpu.TPUConfig(
           iterations_per_loop=FLAGS.iterations_per_loop,
           num_shards=FLAGS.num_tpu_cores,
